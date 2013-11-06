e079d02ef0c72a54fe29b645b52cab02c6b244a3
mmmpld <richardmolloy@gmail.com>
2013-11-06 19:39:10 +1300

    simple upload

--- dogwalks/code/WalkSuggestForm.php
+++ dogwalks/code/WalkSuggestForm.php
@@ -167,9 +167,8 @@ class WalkSuggestForm extends Form {
         }
         $fileID = $fileObj->ID;
         if (isset($fileID)) {
-          echo $fileID . '{file page}' . $pageID;
           // DB::query("INSERT INTO \"walkpage_images\" SET \"ImageID\"='$fileID', \"WalkPageID\"='$pageID'");
-          DB::query("INSERT INTO walkpage_images (ImageID, WalkPageID) VALUES ('$fileID', '$pageID')";
+          DB::query("INSERT INTO walkpage_images (ImageID, WalkPageID) VALUES ('$fileID', '$pageID')");
         }
         if ($fileObj && !$data['Lat'] && !$data['Lng']) {
           $exifGPS = $this->getExifLonLat($fileObj->getFullPath());
