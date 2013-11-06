7517dbb4ef7d2096f6feeec0f2df93e6c8fa244f
mmmpld <richardmolloy@gmail.com>
2013-11-06 21:58:02 +1300

    update content editors group

--- dogwalks/code/WalkSuggestForm.php
+++ dogwalks/code/WalkSuggestForm.php
@@ -97,7 +97,7 @@ class WalkSuggestForm extends Form {
        */
       foreach ($UploadedImages as $file) {
         $parentID = 129; // assets/walks/submitted/ folder
-        $ownerID = 2; // content editors group
+        $ownerID = 1; // content editors group
         $className = 'Image';
         $name = $file;
         $ext = pathinfo($uploadBase.$uploadTmpDir.$file, PATHINFO_EXTENSION);
